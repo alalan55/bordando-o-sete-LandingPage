a6c3c36adb54d72a9de8288afc55c070c8fa0c1f
Alan Oliveira <alla.ao4975@gmail.com>
2021-05-20 21:31:06 -0300

    versão alfa v1

---
+++ script/script.js
@@ -0,0 +1,10 @@
+scroolTop = () => {
+    window.scrollTo(0, 0)
+ }
+ fechaNavbar = () => {
+    let ul = document.querySelector("ul");
+    let check = document.querySelector("#check");
+    ul.style.left = "100%";
+    check.checked = false;
+ }
+ 
